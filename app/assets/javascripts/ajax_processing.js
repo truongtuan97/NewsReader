30354ad694ce3656e6bccad4a3529fc4707548e8
NGOC TUAN TRUONG <truongtuan97@yahoo.com>
2020-01-07 15:06:36 +0700

    implement css

--- app/assets/javascripts/ajax_processing.js
+++ app/assets/javascripts/ajax_processing.js
@@ -14,8 +14,17 @@ function getData(url) {
     dataType: 'json',
     complete: function(data) {      
       var doc = data.responseText;
-      var content = $(doc).find('table .itemlist');       
-      $('#content').html(content);
+      var table = $(doc).find('table .itemlist');    
+      var titles = [];
+      var urls = [];
+
+      getUrlsAndTitles(table, titles, urls);
+    
+      var string_html = processDisplayContent(titles, urls);
+
+      $('#content').html(string_html);
+
+      setRandomColor();
     },
     error: function(error) {
       console.log("error: ", error);
@@ -23,3 +32,34 @@ function getData(url) {
   });
 }
 
+function getUrlsAndTitles(contentTable, titles, urls) {  
+  var tbody = $(contentTable).children('tbody');
+  var trs = $(tbody).children('tr .athing');
+  for (var i=0; i< trs.length; i++) {
+    var title_el = $(trs[i]).children('td .title');
+    var a_tag = $(title_el).children('a');
+    titles.push($(a_tag).text());
+    if ($(a_tag).attr('href').indexOf('http') < 0)
+      urls.push('https://news.ycombinator.com/' + $(a_tag).attr('href'));
+    else
+      urls.push($(a_tag).attr('href'));
+  }  
+}
+
+function processDisplayContent(titles, urls) {
+  var html_str = '';
+  for (var i = 0; i < titles.length; i++) {
+    html_str += "<div class='col-sm-3 single-post-card'>";
+    html_str +=   "<div class='card'>";
+    html_str +=     "<div class='card-block'>";
+    html_str +=         "<a href='" + urls[i] + "' class='interested'>"     
+    html_str +=           "<h4 class='post-text'>" + titles[i] + "</h4>"
+    html_str +=         "</a>"
+    html_str +=       "<div class='post-content'>"    
+    html_str +=       "</div>"
+    html_str +=     "</div>"
+    html_str +=   "</div>"
+    html_str += "</div>"
+  }
+  return html_str;
+}
